2f73d41d3ec2219ea78af0036e961243dd80d7e1
nathalie7980 <londonhippos@gmail.com>
2023-04-09 20:35:47 +0800

    added main viewmodel

--- app/src/main/java/com/nathalie/replybot/MainActivity.kt
+++ app/src/main/java/com/nathalie/replybot/MainActivity.kt
@@ -3,8 +3,11 @@ package com.nathalie.replybot
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -15,14 +18,16 @@ import com.google.android.material.button.MaterialButton
 import com.google.android.material.navigation.NavigationView
 import com.nathalie.replybot.service.AuthService
 import com.nathalie.replybot.utils.Constants
+import com.nathalie.replybot.viewModel.MainActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
-
+    private val viewModel: MainActivityViewModel by viewModels()
 
     @Inject
     lateinit var authRepo: AuthService
@@ -34,8 +39,6 @@ class MainActivity : AppCompatActivity() {
         val btnLogout = findViewById<MaterialButton>(R.id.btnLogout)
 
         makeDrawerLayout(drawerLayout)
-
-        Log.d(Constants.DEBUG, authRepo.isLoggedIn().toString())
 
         if (!authRepo.isLoggedIn()) {
             findNavController(R.id.navHostFragment).navigate(R.id.to_login_fragment)
@@ -54,6 +57,17 @@ class MainActivity : AppCompatActivity() {
         val navigationView = findViewById<NavigationView>(R.id.navigationView)
         navigationView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    fun setUsername() {
+        lifecycleScope.launch {
+            viewModel.getCurrentUser()
+        }
+
+        viewModel.user.observe(this) { user ->
+            val username = findViewById<TextView>(R.id.tvUserName)
+            username.text = "@${user.name}"
+        }
     }
 
     fun logout(drawerLayout: DrawerLayout) {
